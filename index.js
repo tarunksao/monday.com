f3a7fec4d2499c6f455ee9bc9c3aed1b4d5113d7
Tarun Kumar Sao <tarun.k.sao@gmail.com>
2022-07-21 02:56:47 +0530

    Added Day2 progress work

---
+++ index.js
@@ -0,0 +1,13 @@
+let dropdown = document.querySelector(".dropdown");;
+dropdown.addEventListener("click", function() {
+    dropdown.classList.toggle("active");
+});
+
+
+let btn = document.querySelector("#nav button");
+btn.addEventListener("click", popWindow);
+function popWindow()
+{
+    
+}
+
